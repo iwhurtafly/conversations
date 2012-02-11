45d41d9ab1a80eca7d5f741fbbbae1259b727f69
Takashi Hayasaka <iwhurtafly@gmail.com>
2012-02-11 22:44:31 +0900

    mend

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require 'rails/all'
 # takashi add 2012/02/11
 # Pick the frameworks you want:
 # require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+#require "action_controller/railtie"
+#require "action_mailer/railtie"
+#require "active_resource/railtie"
+#require "sprockets/railtie"
+## require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
