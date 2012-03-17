87cb9b3bd0bc0b9501d064f202d9aeba0e6eeb94
Takashi Hayasaka <iwhurtafly@gmail.com>
2012-03-17 23:26:00 +0900

    minor change 2012/03/17

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -14,6 +14,7 @@ class ConversationsController < ApplicationController
       @user1 = Array.new
       @user1.push("#{user1['name']}", "#{user1['screen_name']}", "#{user1['location']}", "#{user1['description']}", "#{user1['url']}")
       @user1_img = Twitter.profile_image(params[:search_string1], :size => 'bigger')
+      @user1_recent_tweet = Twitter.user_timeline(params[:search_string1]).first.text
     rescue Twitter::Error::NotFound
       flash[:notice] = 'We couldn\'t find that user1...'
     rescue Twitter::Error::BadGateway, Twitter::Error::BadRequest, Twitter::Error::EnhanceYourCalm, Twitter::Error::Forbidden, Twitter::Error::InternalServerError, Twitter::Error::NotAcceptable, Twitter::Error::ServiceUnavailable, Twitter::Error::Unauthorized
@@ -25,6 +26,7 @@ class ConversationsController < ApplicationController
       @user2 = Array.new
       @user2.push("#{user2['name']}", "#{user2['screen_name']}", "#{user2['location']}", "#{user2['description']}", "#{user2['url']}")
       @user2_img = Twitter.profile_image(params[:search_string2], :size => 'bigger')
+      @user2_recent_tweet = Twitter.user_timeline(params[:search_string2]).first.text
     rescue Twitter::Error::NotFound
       flash[:notice] = 'We couldn\'t find that user2...'
     rescue Twitter::Error::BadGateway, Twitter::Error::BadRequest, Twitter::Error::EnhanceYourCalm, Twitter::Error::Forbidden, Twitter::Error::InternalServerError, Twitter::Error::NotAcceptable, Twitter::Error::ServiceUnavailable, Twitter::Error::Unauthorized
@@ -36,28 +38,36 @@ class ConversationsController < ApplicationController
     flash[:notice] = ""
     begin
       reply1 = Array.new
-      #Twitter.user_timeline(params[:user1_screen_name], options = {:count=>"200", :page=>"1"}).each do |twitter|
-      Twitter.user_timeline(params[:search_string1], options = {:count=>"200", :page=>"1"}).each do |twitter|
-        text = "#{twitter['text']}"
-        #if text =~ /^@#{params[:user2_screen_name]}/
-        if text =~ /^@#{params[:search_string2]}/
-          reply1.push(["#{twitter['created_at']}", "#{twitter['text']}"])
+      
+      i = 0
+      #3200Tweets are MAX.
+      16.times {
+        i += 1
+        Twitter.user_timeline(params[:search_string1], options = {:count=>"200", :page=> i}).each do |twitter|
+          text = "#{twitter['text']}"
+          if text =~ /^@#{params[:search_string2]}/
+            reply1.push(["#{twitter['created_at']}", "#{twitter['text']}"])
+          end
         end
-      end
+      }
     rescue Twitter::Error::BadGateway, Twitter::Error::BadRequest, Twitter::Error::EnhanceYourCalm, Twitter::Error::Forbidden, Twitter::Error::InternalServerError, Twitter::Error::NotAcceptable, Twitter::Error::NotFound, Twitter::Error::ServiceUnavailable, Twitter::Error::Unauthorized
       flash[:notice] = 'Maybe Twitter is down... Or check the name you input.'
     end
 
     begin    
       reply2 = Array.new
-      #Twitter.user_timeline(params[:user2_screen_name], options = {:count=>"200", :page=>"1"}).each do |twitter|
-      Twitter.user_timeline(params[:search_string2], options = {:count=>"200", :page=>"1"}).each do |twitter|
-        text = "#{twitter['text']}"
-        #if text =~ /^@#{params[:user1_screen_name]}/
-        if text =~ /^@#{params[:search_string1]}/
-          reply2.push(["#{twitter['created_at']}", "#{twitter['text']}"])
+
+      i = 0
+      #3200Tweets are MAX.
+      16.times {
+        i += 1
+        Twitter.user_timeline(params[:search_string2], options = {:count=>"200", :page=> i}).each do |twitter|
+          text = "#{twitter['text']}"
+          if text =~ /^@#{params[:search_string1]}/
+            reply2.push(["#{twitter['created_at']}", "#{twitter['text']}"])
+          end
         end
-      end
+      }
     rescue Twitter::Error::BadGateway, Twitter::Error::BadRequest, Twitter::Error::EnhanceYourCalm, Twitter::Error::Forbidden, Twitter::Error::InternalServerError, Twitter::Error::NotAcceptable, Twitter::Error::NotFound, Twitter::Error::ServiceUnavailable, Twitter::Error::Unauthorized
       flash[:notice] = 'Maybe Twitter is down... Or check the name you input.'
     end
